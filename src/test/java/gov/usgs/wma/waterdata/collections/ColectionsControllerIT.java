f69adec97a25ca45cc41da1aebde289cb8639928
Orosz <eorosz@contactor.usgs.gov>
2020-02-13 12:09:07 -0600

    IOW-287/IOW-288 (Generate an OGCAPI Features /collections/ service in the observations system)   Removed unused import

--- src/test/java/gov/usgs/wma/waterdata/collections/ColectionsControllerIT.java
+++ src/test/java/gov/usgs/wma/waterdata/collections/ColectionsControllerIT.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
