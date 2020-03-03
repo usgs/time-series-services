dd958c1c49d67555aeaa2f7b30fa8cd37429088a
Orosz <eorosz@contactor.usgs.gov>
2020-03-03 12:36:41 -0600

    IOW-289 (Generate an OGCAPI-Features implementation of the collections/collectionID/items endpoint)   code cleanup

--- src/test/java/gov/usgs/wma/waterdata/collections/FeaturesParamsIT.java
+++ src/test/java/gov/usgs/wma/waterdata/collections/FeaturesParamsIT.java
@@ -15,7 +15,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
