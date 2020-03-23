e4e2f7a4076493d585bce916ee097b6d3d06907e
Orosz <eorosz@contactor.usgs.gov>
2020-03-23 11:10:05 -0500

    IOW-145 (adding links to features collections)   Refactored to address ticket update by product owner (addressing on getting 1000 items).   Collection links are now only added to the feature json on sigle item get.

--- src/test/java/gov/usgs/wma/waterdata/collections/FeaturesParamsIT.java
+++ src/test/java/gov/usgs/wma/waterdata/collections/FeaturesParamsIT.java
@@ -2,7 +2,6 @@ package gov.usgs.wma.waterdata.collections;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
