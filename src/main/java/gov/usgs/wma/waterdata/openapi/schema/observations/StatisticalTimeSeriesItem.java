7ebb0beb3c397fe92209a59241d1452c2e4bc372
eorosz <eorosz@contractor.usgs.gov>
2021-06-09 08:51:14 -0400

    IOW-965 -- Added best field to openapi StatisticalTimeSeriesItem

--- src/main/java/gov/usgs/wma/waterdata/openapi/schema/observations/StatisticalTimeSeriesItem.java
+++ src/main/java/gov/usgs/wma/waterdata/openapi/schema/observations/StatisticalTimeSeriesItem.java
@@ -4,6 +4,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Statistical time series description entry.")
 public class StatisticalTimeSeriesItem {
+	public static final String BEST_DESC = "The time series deemed the \"best\" of those currently served at this station and "
+			+ "has the longest-running record out of many parameter and statistic combinations. "
+			+ "The \"best\" time series is intended to help users identify the largest "
+			+ "and most reliable data set at a site when only one value per day is needed.";
 
 	@Schema(description = "The time series unique identifier.", example = "AGENCY-FEATURE-TIMESERIES")
     public String getId() {
@@ -22,6 +26,11 @@ public class StatisticalTimeSeriesItem {
 
 	@Schema(description = "The five digit statistic code unique identifier.", example = "00002")
     public String getStatisticCode() {
+		return null;
+	}
+
+	@Schema(description = BEST_DESC, example = "true")
+	public Boolean getBest() {
 		return null;
 	}
 
